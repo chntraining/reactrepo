ba8cd0620c5fb59fb03282e0d2b607fa677d5ddc
Deeps <deepikkaa20@gmail.com>
2024-11-14 23:04:13 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px", backgroundColor: "pink" }}>
+      <h1 style={{ color: "blue" }}>Welcome to React App on Kubernetes!</h1>
+      <p>This is a simple React application deployed on Kubernetes.</p>
+    </div>
+  );
+}
+
+export default App;
